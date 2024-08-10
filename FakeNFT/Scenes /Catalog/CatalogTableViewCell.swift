63d2b7a344ebeb45e523c78040dde829f0c06912
glomebom <gleb.khodosevich@gmail.com>
2024-08-10 13:46:37 +0300

    Написать класс ячейки для каталога

---
+++ FakeNFT/Scenes /Catalog/CatalogTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  CatalogTableViewCell.swift
+//  FakeNFT
+//
+//  Created by Gleb on 10.08.2024.
+//
+
+import UIKit
+
+final class CatalogTableViewCell: UITableViewCell, ReuseIdentifying {
+    private lazy var catalogImage: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = 12
+        image.clipsToBounds = true
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    private lazy var catalogLabel: UILabel = {
+        let label = UILabel()
+        label.font = .bodyBold
+        label.textColor = .textPrimary
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        setupCatalogTableViewCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCatalogTableViewCell() {
+        // TODO: реализовать элементы на экране католога
+        backgroundColor = .clear
+        selectionStyle = .none
+        addSubviews()
+        setupCatalogTableViewCellConstrains()
+    }
+    
+    private func addSubviews() {
+        [catalogImage, catalogLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview($0)
+        }
+    }
+    
+    private func setupCatalogTableViewCellConstrains() {
+        NSLayoutConstraint.activate([
+            catalogImage.heightAnchor.constraint(equalToConstant: 140),
+            catalogImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 64),
+            catalogImage.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            catalogImage.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            catalogLabel.topAnchor.constraint(equalTo: catalogImage.bottomAnchor, constant: 4),
+            catalogLabel.bottomAnchor.constraint(equalTo: catalogImage.bottomAnchor, constant: -13),
+            catalogLabel.leadingAnchor.constraint(equalTo: catalogImage.leadingAnchor),
+            catalogLabel.trailingAnchor.constraint(equalTo: catalogImage.trailingAnchor)
+        ])
+    }
+}
---
+++ FakeNFT/Scenes /Catalog/CatalogViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CatalogViewController.swift
+//  FakeNFT
+//
+//  Created by Gleb on 10.08.2024.
+//
+
+import UIKit
+
+final class CatalogViewController: UIViewController {
+    
+}
