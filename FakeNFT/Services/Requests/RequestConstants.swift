e99cc9a654aa543d0167695b1a0d2b94ed6692c1
glomebom <gleb.khodosevich@gmail.com>
2024-08-12 23:53:07 +0300

    Добавить токен

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,6 +1,6 @@
 enum RequestConstants {
     #warning("insert your baseUrl <id>.mockapi.io")
     static let baseURL = ""
-    static let accessToken = ""
+    static let accessToken = "4f7ff936-e771-4f17-ac34-c7378a7bcf7b"
     static let requestKey = "X-Practicum-Mobile-Token"
 }
