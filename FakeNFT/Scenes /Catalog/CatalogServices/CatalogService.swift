124048aed4e434cc7ff23aab980500d0ac134ff5
glomebom <gleb.khodosevich@gmail.com>
2024-08-13 14:48:36 +0300

    Убрать лишние комментарии

--- FakeNFT/Scenes /Catalog/CatalogServices/CatalogService.swift
+++ FakeNFT/Scenes /Catalog/CatalogServices/CatalogService.swift
@@ -20,30 +20,16 @@ final class CatalogService: CatalogServiceProtocol {
     
     func getNftCollections(completion: @escaping (Result<[NFTCollection], any Error>) -> Void) {
         let request = NFTCollectionsRequest()
-        
-        ///
-        print("[DEBUG]: request: \(request)")
-        ///
-        
         networkClient.send(
             request: request,
             type: [NFTCollection].self) { result in
                 switch result {
                 case .success(let collections):
-                    ///
-                    print("\(collections)")
-                    ///
                     completion(.success(collections))
                 case .failure(let error):
                     if let networkError = error as? NetworkClientError {
-                        ///
-                        print("[DEBUG]: [ERROR]: CatalogService: networkClient.send")
-                        ///
                         completion(.failure(networkError))
                     } else {
-                        ///
-                        print("[DEBUG]: [ERROR]: CatalogService: networkClient.send")
-                        ///
                         completion(.failure(error))
                     }
                 }
