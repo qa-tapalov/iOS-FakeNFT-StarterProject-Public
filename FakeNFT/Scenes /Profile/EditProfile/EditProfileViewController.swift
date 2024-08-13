fa34ebba7a3f90918bf40ec87b32348be83becbc
yurii kleimenov <yurakleimenovv@gmail.com>
2024-08-13 21:27:08 +0200

    Fix overlay label

--- FakeNFT/Scenes /Profile/EditProfile/EditProfileViewController.swift
+++ FakeNFT/Scenes /Profile/EditProfile/EditProfileViewController.swift
@@ -7,14 +7,18 @@
 
 import UIKit
 
+protocol EditProfileViewProtocol: AnyObject {
+    func display(data: EditProfileScreenModel, reloadTableData: Bool)
+}
+
 final class EditProfileViewController: UIViewController {
-    
+
     // MARK: - Properties
-    
+
     typealias Cell = EditProfileScreenModel.TableData.Cell
-    
+
     // MARK: - UI Elements
-    
+
     private lazy var backButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "multiply"), for: .normal)
@@ -22,7 +26,7 @@ final class EditProfileViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-    
+
     private lazy var avatarImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
@@ -31,18 +35,18 @@ final class EditProfileViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-    
+
     private lazy var avatarLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
         label.textAlignment = .center
         label.numberOfLines = 0
-        label.text = "Cменить /nфото"
+        label.text = "Cменить \nфото"
         label.font = UIFont.systemFont(ofSize: 10, weight: .medium)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     private lazy var avatarOverlay: UIView = {
         let view = UIView()
         view.clipsToBounds = true
@@ -52,32 +56,32 @@ final class EditProfileViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .insetGrouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
-    
+
     private var model: EditProfileScreenModel = .empty {
         didSet {
             setup()
         }
     }
-    
+
     // MARK: - Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
-    
+
     // MARK: - Private methods
-    
+
     private func setup() {
         avatarImageView.image = model.image
     }
-    
+
     private func setupView() {
         view.backgroundColor = .white
         view.addSubview(tableView)
@@ -87,7 +91,7 @@ final class EditProfileViewController: UIViewController {
         configureAvatarImageView()
         configureBackButton()
     }
-    
+
     private func setupTableView() {
         tableView.backgroundColor = .white
         tableView.delegate = self
@@ -95,7 +99,7 @@ final class EditProfileViewController: UIViewController {
         tableView.register(TextViewCell.self, forCellReuseIdentifier: TextViewCell.identifier)
         tableView.register(TextFieldCell.self, forCellReuseIdentifier: TextFieldCell.identifier)
         tableView.register(TableHeaderView.self, forHeaderFooterViewReuseIdentifier: TableHeaderView.identifier)
-        
+
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: Constants.tableViewTopInsets),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.horizont),
@@ -103,27 +107,27 @@ final class EditProfileViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -Constants.horizont)
         ])
     }
-    
+
     private func configureAvatarImageView() {
         avatarImageView.addSubview(avatarOverlay)
         avatarOverlay.addSubview(avatarLabel)
-        
+
         NSLayoutConstraint.activate([
             avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.avatarTopOffset),
             avatarImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             avatarImageView.widthAnchor.constraint(equalToConstant: Constants.avatarSize),
             avatarImageView.heightAnchor.constraint(equalToConstant: Constants.avatarSize),
-            
-            avatarOverlay.topAnchor.constraint(equalTo: view.topAnchor),
-            avatarOverlay.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            avatarOverlay.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            avatarOverlay.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
-            avatarLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            avatarLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+
+            avatarOverlay.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            avatarOverlay.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+            avatarOverlay.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            avatarOverlay.trailingAnchor.constraint(equalTo: avatarImageView.trailingAnchor),
+
+            avatarLabel.centerXAnchor.constraint(equalTo: avatarOverlay.centerXAnchor),
+            avatarLabel.centerYAnchor.constraint(equalTo: avatarOverlay.centerYAnchor)
         ])
     }
-    
+
     private func configureBackButton() {
         NSLayoutConstraint.activate([
             backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.backButtonTop),
@@ -131,26 +135,26 @@ final class EditProfileViewController: UIViewController {
         ])
         backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
     }
-    
+
     private func tableDataCell(indexPath: IndexPath) -> Cell {
         let section = model.tableData.sections[indexPath.section]
-        
+
         switch section {
         case .headeredSection(_, cells: let cells):
             return cells[indexPath.row]
         }
     }
-    
+
     private func configureTapGesture() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
     }
-    
+
     @objc private func hideKeyboard() {
         view.endEditing(true)
     }
-    
+
     @objc private func backButtonTapped() {
         // TODO: - Add changes save logic
         dismiss(animated: true)
@@ -172,7 +176,7 @@ extension EditProfileViewController: UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         model.tableData.sections.count
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch model.tableData.sections[section] {
         case .headeredSection(_, cells: let cells):
@@ -187,7 +191,7 @@ extension EditProfileViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellType = tableDataCell(indexPath: indexPath)
         let cell: UITableViewCell
-        
+
         switch cellType {
         case let .textViewCell(model):
             guard let textViewCell = tableView.dequeueReusableCell(withIdentifier: TextViewCell.identifier, for: indexPath) as? TextViewCell else { return UITableViewCell() }
@@ -200,27 +204,27 @@ extension EditProfileViewController: UITableViewDataSource {
         }
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+
     }
-    
+
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: TableHeaderView.identifier) as? TableHeaderView else {
             return nil
         }
-        
+
         let headerText: String
-        
+
         switch model.tableData.sections[section] {
         case .headeredSection(header: let header, _):
             headerText = header
         }
-        
+
         headerView.setup(with: headerText)
         return headerView
     }
-    
+
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return UITableView.automaticDimension
     }
--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
-    
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net/"
     static let token = "4f7ff936-e771-4f17-ac34-c7378a7bcf7b"
 }
