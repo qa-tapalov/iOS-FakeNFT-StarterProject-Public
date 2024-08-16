521920bd5130780597b9af669f48b82c45cb37e7
Artur <sssk1tlll@yandex.ru>
2024-08-16 19:51:23 +0300

    настройка отображения статистики

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -34,20 +34,32 @@ final class TabBarController: UITabBarController {
     private func setupTabBar() {
         view.backgroundColor = UIColor.systemBackground
         
-        //        let profileViewController = UINavigationController(rootViewController: )
-        //        profileViewController.tabBarItem = profileTabBarItem
-        //
-        //        let catalogViewController = UINavigationController(rootViewController: )
-        //        catalogViewController.tabBarItem = catalogTabBarItem
-        //
-        //        let cartViewController = UINavigationController(rootViewController: )
-        //        cartViewController.tabBarItem = cartTabBarItem
-        //
-        //        let statisticViewController = UINavigationController(rootViewController: )
-        //        statsViewController.tabBarItem = statisticsTabBarItem
-        //
-        //        viewControllers = [profileViewController, catalogViewController, cartViewController, statisticViewController]
-        //
+//        let profileViewController = UINavigationController(rootViewController: ProfileViewController())
+//        profileViewController.tabBarItem = profileTabBarItem
+//        
+//        let catalogViewController = UINavigationController(rootViewController: CatalogViewController())
+//        catalogViewController.tabBarItem = catalogTabBarItem
+//        
+//        let cartViewController = UINavigationController(rootViewController: CartViewController())
+//        cartViewController.tabBarItem = cartTabBarItem
         
+        let statsViewController = UINavigationController(rootViewController: StatisticsViewController())
+        statsViewController.tabBarItem = statisticsTabBarItem
+        
+        viewControllers = [/*profileViewController, catalogViewController, cartViewController,*/ statsViewController]
+        
+        let tabBarAppearance = tabBar.standardAppearance
+        tabBarAppearance.configureWithDefaultBackground()
+        tabBarAppearance.backgroundColor = .ypWhite
+        
+        let normalAppearance = UITabBarItemAppearance()
+        normalAppearance.normal.iconColor = .ypBlack
+        normalAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.ypBlack ?? UIColor()]
+        
+        tabBarAppearance.stackedLayoutAppearance = normalAppearance
+        tabBarAppearance.inlineLayoutAppearance = normalAppearance
+        tabBarAppearance.compactInlineLayoutAppearance = normalAppearance
+        
+        tabBar.standardAppearance = tabBarAppearance
     }
 }
